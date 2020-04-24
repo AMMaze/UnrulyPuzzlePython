c35f89cb8a1100305a2e76df1af52541c34f5ed0
Marat Abbas <unlock96@gmail.com>
2020-04-24 16:38:10 +0300

    style fixes

--- gui/styles/Custom_Button.py
+++ gui/styles/Custom_Button.py
@@ -1,11 +1,10 @@
 import tkinter as tk
 from tkinter import ttk
-from PIL import Image, ImageDraw, ImageFont, ImageTk, features
+from PIL import Image, ImageDraw, ImageFont, ImageTk
 import textwrap
 from time import sleep
 from queue import Queue
 from threading import Thread
-from tkinter import font as tkFont
 
 
 class Round_Button(tk.Label):
@@ -45,7 +44,7 @@ class Round_Button(tk.Label):
         self.tsc = static_t_colour
         self.ttc = transformation_t_colour
         self.multi = size
-        self.resoltuion = (int(35*size), int(10*size))  # 3.5 : 1 (W : H)
+        self.resolution = (int(35*size), int(10*size))  # 3.5 : 1 (W : H)
         self.text = text
         self.change_to_trans = False
         self.change_to_static = False
@@ -76,124 +75,182 @@ class Round_Button(tk.Label):
             # < decrement > : Used for lowering the font size so that the
             #   text doesn't go off the screen.
             decrement += 1
-            font = ImageFont.truetype('gui/styles/Assets/roboto/Roboto-Regular.ttf',
-                                      int(4 * self.multi) - decrement, encoding="unic")
+            font = ImageFont.truetype(
+                'gui/styles/Assets/roboto/Roboto-Regular.ttf',
+                int(4 * self.multi) - decrement,
+                encoding="unic")
             coords, Lines, line_height = self.draw_multiple_line_text(
-                self.text, font, int(36 * self.multi), int(2.2 * self.multi), 12)
-            if coords[-1][1] + line_height + 5 > self.resoltuion[1]:
+                self.text, font, int(36 * self.multi),
+                int(2.2 * self.multi), 12)
+            if coords[-1][1] + line_height + 5 > self.resolution[1]:
                 continue
             break
 
-        self.images = [Image.new('RGBA', (self.resoltuion)) for i in range(10)]
+        self.images = [Image.new('RGBA', (self.resolution)) for i in range(10)]
 
         # Initialising the draw the ImageDraw.Draw object
         self.image_drawer = [ImageDraw.Draw(self.images[i]) for i in range(10)]
         self.image_colours = [
-            [self.tc[i] + ((self.sc[i]-self.tc[i])//10)*x for i in range(3)] for x in range(10)]
+            [self.tc[i] + ((self.sc[i]-self.tc[i])//10)*x for i in range(3)]
+            for x in range(10)]
         self.text_colours = [
-            [self.ttc[i] + ((self.tsc[i] - self.ttc[i]) // 10) * x for i in range(3)] for x in range(10)]
+            [self.ttc[i] + ((self.tsc[i] - self.ttc[i]) // 10)
+             * x for i in range(3)] for x in range(10)]
         self.outline_colours = [[self.trans_outline[i] + (
-            (self.static_outline[i] - self.trans_outline[i]) // 10) * x for i in range(3)] for x in range(10)]
+            (self.static_outline[i] - self.trans_outline[i]) // 10)
+            * x for i in range(3)] for x in range(10)]
         for i in range(10):
 
             # Puts the colours in a tuple for use.
-            colour = (self.image_colours[i][0], self.image_colours[i][1], self.image_colours[i][2])
-            textcolour = (self.text_colours[i][0], self.text_colours[i][1], self.text_colours[i][2])
+            colour = (
+                self.image_colours[i][0],
+                self.image_colours[i][1],
+                self.image_colours[i][2]
+            )
+            textcolour = (
+                self.text_colours[i][0],
+                self.text_colours[i][1],
+                self.text_colours[i][2]
+            )
             outline = (self.outline_colours[i][0],
                        self.outline_colours[i][1], self.outline_colours[i][2])
 
             # Creates the base for both images (Rectangles)
 
-            self.image_drawer[i].rectangle((int(5.5 * self.multi), 0, self.resoltuion[0] - int(
-                5.5 * self.multi), self.resoltuion[1]-1), outline=outline, width=2, fill=colour)
+            self.image_drawer[i].rectangle(
+                (int(5.5 * self.multi), 0,
+                 self.resolution[0] - int(5.5 * self.multi),
+                 self.resolution[1]-1), outline=outline,
+                width=2, fill=colour
+            )
 
-            # Create a rectangle to remove the unwanted areas of colour, and adds an elipses to give a round effect.
+            # Create a rectangle to remove the unwanted areas of colour,
+            # and adds an elipses to give a round effect.
             # 2 on both sides for 2 images.
 
             self.image_drawer[i].rectangle(
-                (self.resoltuion[0] - int(5.5 * self.multi), 0, self.resoltuion[0], self.resoltuion[1]-2), fill=(0, 0, 0, 0))
-            self.image_drawer[i].ellipse((self.resoltuion[0] - int(10 * self.multi), 0,
-                                          self.resoltuion[0]-1, self.resoltuion[1]-2), outline=outline, width=2, fill=colour)
-
-            self.image_drawer[i].rectangle(
-                (0, 0, int(5.5 * self.multi), int(10 * self.multi)-2), fill=(0, 0, 0, 0))
+                (self.resolution[0] - int(5.5 * self.multi), 0,
+                 self.resolution[0], self.resolution[1]-2),
+                fill=(0, 0, 0, 0)
+            )
             self.image_drawer[i].ellipse(
-                (0, 0, int(10 * self.multi), int(10 * self.multi)-2), outline=outline, width=2, fill=(colour))
+                (self.resolution[0] - int(10 * self.multi), 0,
+                 self.resolution[0]-1, self.resolution[1]-2),
+                outline=outline, width=2, fill=colour
+            )
 
             self.image_drawer[i].rectangle(
-                (int(5.5 * self.multi), 2, self.resoltuion[0] - int(5.5 * self.multi), self.resoltuion[1]-3), fill=colour)
+                (0, 0, int(5.5 * self.multi), int(10 * self.multi)-2),
+                fill=(0, 0, 0, 0)
+            )
+            self.image_drawer[i].ellipse(
+                (0, 0, int(10 * self.multi), int(10 * self.multi)-2),
+                outline=outline, width=2, fill=(colour)
+            )
+
+            self.image_drawer[i].rectangle(
+                (int(5.5 * self.multi), 2,
+                 self.resolution[0] - int(5.5 * self.multi),
+                 self.resolution[1]-3), fill=colour
+            )
 
             for x in range(len(coords)):
                 self.image_drawer[i].text(
-                    coords[x], Lines[x], fill=textcolour, font=font, align='center')
+                    coords[x], Lines[x], fill=textcolour,
+                    font=font, align='center'
+                )
 
         self.Images = [ImageTk.PhotoImage(self.images[i]) for i in range(10)]
 
     def create_lower_button(self):
         multi_d = 0.25
         multi = self.multi - multi_d
-        resoltuion = (int(35 * multi), int(10*multi))
+        resolution = (int(35 * multi), int(10*multi))
         decrement = -1
         while True:
-            # < decrement > : Used for lowering the font size so that the text doesn't go off the screen.
+            # < decrement > : Used for lowering the font size so
+            # that the text doesn't go off the screen.
             decrement += 1
             font = ImageFont.truetype('gui/styles/Assets/Roboto-Regular.ttf',
-                                      int(4 * multi) - decrement, encoding="unic")
+                                      int(4 * multi) - decrement,
+                                      encoding="unic")
             coords, Lines, line_height = self.draw_multiple_line_text(
                 self.text, font, int(36 * multi), int(2.2 * multi), 12)
-            if coords[-1][1] + line_height + 5 > self.resoltuion[1]-(10*multi_d):
+            if coords[-1][1]+line_height + 5 > self.resolution[1]-(10*multi_d):
                 continue
             break
 
-        self.lower_button = Image.new('RGBA', (resoltuion))
+        self.lower_button = Image.new('RGBA', (resolution))
 
         # Initialising the draw the ImageDraw.Draw object
         self.lower_drawer = ImageDraw.Draw(self.lower_button)
 
-        colour = (self.image_colours[0][0], self.image_colours[0][1], self.image_colours[0][2])
-        textcolour = (self.text_colours[0][0], self.text_colours[0][1], self.text_colours[0][2])
+        colour = (
+            self.image_colours[0][0],
+            self.image_colours[0][1],
+            self.image_colours[0][2]
+        )
+        textcolour = (
+            self.text_colours[0][0],
+            self.text_colours[0][1],
+            self.text_colours[0][2]
+        )
         outline = (self.outline_colours[0][0], self.outline_colours[0]
                    [1], self.outline_colours[0][2])
 
         # Creates the base for both images (Rectangles)
 
-        # Create a rectangle to remove the unwanted areas of colour, and adds an elipses to give a round effect.
+        # Create a rectangle to remove the unwanted areas of colour,
+        # and adds an elipses to give a round effect.
         # 2 on both sides for 2 images.
 
         self.lower_drawer.rectangle(
-            (0, 0, resoltuion[0], resoltuion[1]-1), outline=outline, width=2,  fill=colour)
+            (0, 0, resolution[0], resolution[1]-1),
+            outline=outline, width=2,  fill=colour
+        )
 
-        # Create a rectangle to remove the unwanted areas of colour, and adds an elipses to give a round effect.
+        # Create a rectangle to remove the unwanted areas of colour,
+        # and adds an elipses to give a round effect.
         # 2 on both sides for 2 images.
 
         # Right side
         self.lower_drawer.rectangle(
-            (resoltuion[0] - int(5.5*multi), 0, resoltuion[0], resoltuion[1]), fill=(0, 0, 0, 0))
+            (resolution[0] - int(5.5*multi), 0,
+             resolution[0], resolution[1]), fill=(0, 0, 0, 0)
+        )
         self.lower_drawer.ellipse(
-            (resoltuion[0] - int(10*multi), 0, resoltuion[0], resoltuion[1]), outline=outline, width=2, fill=colour)
+            (resolution[0] - int(10*multi), 0, resolution[0], resolution[1]),
+            outline=outline, width=2, fill=colour
+        )
 
         # Left side
-        self.lower_drawer.rectangle((0, 0, int(5.5 * multi), int(10 * multi)), fill=(0, 0, 0, 0))
+        self.lower_drawer.rectangle(
+            (0, 0, int(5.5 * multi), int(10 * multi)), fill=(0, 0, 0, 0))
         self.lower_drawer.ellipse((0, 0, int(10 * multi), int(10 * multi)),
                                   outline=outline, width=2, fill=(colour))
 
         self.lower_drawer.rectangle(
-            (int(5.5 * multi), 2, resoltuion[0] - int(5.5*multi), resoltuion[1]-3), fill=colour)
+            (int(5.5 * multi), 2, resolution[0] - int(5.5*multi),
+             resolution[1]-3), fill=colour
+        )
 
         for x in range(len(coords)):
-            self.lower_drawer.text(coords[x], Lines[x], fill=textcolour, font=font, align='center')
+            self.lower_drawer.text(
+                coords[x], Lines[x], fill=textcolour,
+                font=font, align='center'
+            )
 
-        delta_x = (self.resoltuion[0] - resoltuion[0])//2
-        delta_y = (self.resoltuion[1] - resoltuion[1])//2
+        delta_x = (self.resolution[0] - resolution[0])//2
+        delta_y = (self.resolution[1] - resolution[1])//2
 
         # Perfects the size for pasting.
         self.lower_button = self.lower_button.resize(
-            size=(self.resoltuion[0] - delta_x*2, self.resoltuion[1] - delta_y*2))
+            size=(self.resolution[0] - delta_x*2, self.resolution[1] - delta_y*2))
 
         # Pasting Image ontop of transparent image with original resolution.
-        self.Button = Image.new('RGBA', (self.resoltuion))
+        self.Button = Image.new('RGBA', (self.resolution))
         self.Button.paste(self.lower_button, (delta_x, delta_y,
-                                              self.resoltuion[0] - delta_x, self.resoltuion[1] - delta_y), self.lower_button)
+                                              self.resolution[0] - delta_x, self.resolution[1] - delta_y), self.lower_button)
 
         self.lower_button = ImageTk.PhotoImage(self.Button)
 
@@ -278,43 +335,3 @@ class Round_Button(tk.Label):
 
         self.bind("<ButtonPress-1>", connector)
         self.bind("<ButtonRelease-1>", disconnector)
-
-
-if __name__ == '__main__':
-    def New_Function():
-        print('Functioning')
-
-    app = tk.Tk()
-    Background = ('#000000')
-    app.configure(background=Background)
-
-    Static_Colour = (0, 0, 0)
-    Text_Transformation_Colour = (255, 255, 255)
-    Transformation_Colour = (0, 0, 0)
-    Text_Static_Colour = (125, 125, 125)
-
-    Static_Outline = (125, 125, 125)
-    Transformation_Outline = (255, 255, 255)
-
-    Button = Round_Button(app, 'Login', 3, Static_Colour, Text_Static_Colour, Transformation_Colour,
-                          Text_Transformation_Colour, Background, Static_Outline, Transformation_Outline)
-    Button.connect_function(New_Function)
-    Button.grid(row=2, column=1, pady=5, padx=10)
-
-    Label1 = ttk.Label(app, text='Username')
-    Label1.configure(background=Background)
-    Label1.configure(foreground=('#FFFFFF'))
-    Label1.grid(row=0, column=0, pady=5, padx=10)
-
-    Entry1 = ttk.Entry(app)
-    Entry1.grid(row=0, column=1, pady=5, padx=10)
-
-    Label2 = ttk.Label(app, text='Password')
-    Label2.configure(background=Background)
-    Label2.configure(foreground=('#FFFFFF'))
-    Label2.grid(row=1, column=0, pady=5, padx=10)
-
-    Entry2 = ttk.Entry(app)
-    Entry2.grid(row=1, column=1, pady=5, padx=10)
-
-    app.mainloop()
