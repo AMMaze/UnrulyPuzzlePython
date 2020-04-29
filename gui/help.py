6e8ef9ba78e8d7f4c97b84523ee432a87a108685
Marat Abbas <unlock96@gmail.com>
2020-04-29 18:03:25 +0300

    inheritance from frame refactoring

--- gui/help.py
+++ gui/help.py
@@ -1,12 +1,12 @@
 import tkinter as tk
 import tkinter.ttk as ttk
-from styles.Custom_Button import Round_Button
-from styles.btn_styles import btn_small_style
+from gui.styles.Custom_Button import Round_Button
+from gui.styles.btn_styles import btn_small_style
 
 # Help Window
 
 
-class Help:
+class Help(tk.Frame):
 
     RULES = """The player is given a grid of cells with size N x M. The color of \
     the cell can be changed by clicking on it. Each square can take one \
@@ -18,23 +18,15 @@ class Help:
     2. No row or column may contain three consecutive squares
      of the same colour."""
 
-    def __init__(self, master):
-
+    def __init__(self, master, controller=None):
+        tk.Frame.__init__(self, master)
+        self.controller = controller
         self.help = master
-        self.help.title("Help")
-        self.help.rowconfigure((0, 1), weight=1)
-        self.help.columnconfigure((0, 1),  weight=1)
-
-        # Main Frame
-
-        self.root_frame = ttk.Frame(self.help)
-        self.root_frame.grid(row=1, column=1, padx=20,
-                             pady=20, sticky=tk.N+tk.E+tk.W+tk.S)
 
         # Define and Put LabelFrame
 
         self.rules_frame = tk.LabelFrame(
-            self.root_frame, text="Rules", font=("Lucida Grande", 18))
+            self, text="Rules", font=("Lucida Grande", 18))
         self.rules_frame.grid(row=0, column=0, padx=5,
                               pady=5, sticky=tk.N+tk.E+tk.W+tk.S)
 
@@ -49,17 +41,14 @@ class Help:
 
         # Buttons
 
-        self.btn_back = Round_Button(self.root_frame, **btn_small_style,
-                                     text="Back")
+        self.btn_back = Round_Button(self, **btn_small_style,
+                                     text="Back", command=lambda: self.controller.show_frame("Main Menu"))
 
         self.btn_back.grid(row=1, column=0, padx=5,
                            pady=5, sticky=tk.S+tk.W+tk.E)
 
         btn_small_style['background'] = ttk.Style().lookup(
             'TFrame', 'background')
-
-        self.help.update()
-        self.help.minsize(self.help.winfo_width(), self.help.winfo_height())
 
 
 if __name__ == "__main__":
