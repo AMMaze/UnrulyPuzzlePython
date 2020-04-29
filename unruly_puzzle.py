a712137b1342a0de455b96cd62039d592d1056ca
Marat Abbas <unlock96@gmail.com>
2020-04-29 19:01:32 +0300

    code refactoring

--- unruly_puzzle.py
+++ unruly_puzzle.py
@@ -10,29 +10,55 @@ class UnrulyPuzzle(tk.Tk):
 
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
+
+        # Root Window Cofiguration
+
         self.rowconfigure(0, weight=1)
         self.columnconfigure(0, weight=1)
 
+        # Current Frame and Dict for Frames
+
         self._frame = None
         self.frames = {}
+
+        # Root Frame Configuration and Placement
+
         self.container = tk.Frame(self)
         self.container.rowconfigure((0, 2), weight=1)
         self.container.columnconfigure((0, 2),  weight=1)
         self.container.grid(row=0, column=0, sticky=tk.N+tk.E+tk.W+tk.S)
+
+        # Frames Dictionary Initialization
+
         self.frames['Main Menu'] = MainMenu
         self.frames['Help'] = Help
         # self.frames['Settings'] = Settings(super(), self)
+
         self.show_frame("Main Menu")
 
     def show_frame(self, page_name):
+        """
+        Shows frame specified by name from dictionary self.frames
+
+        Destroys current frame if present, resizes root window to fit new frame
+
+        :param page_name: name from self.frames dictionary
+        """
+
         frame_class = self.frames[page_name]
         if self._frame is not None:
             self._frame.destroy()
         self.title(page_name)
+
+        # Creating and Configuring New Frame
+
         self._frame = frame_class(self.container, self)
         self._frame.grid(row=1, column=1, padx=20, pady=20,
                          sticky=tk.N+tk.E+tk.W+tk.S)
         self._frame.update()
+
+        # Window Resizing
+
         self.geometry("{}x{}".format(self._frame.winfo_width() +
                                      50, self._frame.winfo_height() + 50))
         self.minsize(self._frame.winfo_width() + 50,
