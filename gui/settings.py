18ac715e52e45f9615b78133a8299c8323ac3afe
Marat Abbas <unlock96@gmail.com>
2020-04-29 19:10:35 +0300

    tk.Frame inheritance refactioring

--- gui/settings.py
+++ gui/settings.py
@@ -1,44 +1,38 @@
 import tkinter as tk
 import tkinter.ttk as ttk
-from styles.Custom_Button import Round_Button
-from styles.btn_styles import btn_small_style
+from gui.styles.Custom_Button import Round_Button
+from gui.styles.btn_styles import btn_small_style
 
 # Settings
 
 
-class Settings:
+class Settings(tk.Frame):
 
-    def __init__(self, master):
+    def __init__(self, master, controller=None):
+        tk.Frame.__init__(self, master)
+        self.controller = controller
+        self.master = master
 
-        self.settings = master
-        self.settings.title("Settings")
-        self.settings.rowconfigure((0, 1), weight=1)
-        self.settings.columnconfigure((0, 1),  weight=1)
+        # Main Frame Configuration
 
-        # Main Frame
-
-        self.root_frame = ttk.Frame(self.settings)
-        self.root_frame.grid(row=1, column=1, padx=20,
-                             pady=20, sticky=tk.N+tk.E+tk.W+tk.S)
-
-        # Frame Grid Configuration
-
-        self.root_frame.grid_rowconfigure(3, minsize=100)
+        self.grid(row=1, column=1, padx=20,
+                  pady=20, sticky=tk.N+tk.E+tk.W+tk.S)
+        self.grid_rowconfigure(3, minsize=100)
 
         # Define and Put Labels
 
         self.lbl_width = ttk.Label(
-            self.root_frame, text='Width:', font=("Lucida Grande", 12))
+            self, text='Width:', font=("Lucida Grande", 12))
         self.lbl_width.grid(row=0, column=0, padx=5,
                             pady=5, sticky=tk.N+tk.W)
 
         self.lbl_height = ttk.Label(
-            self.root_frame, text='Height:', font=("Lucida Grande", 12))
+            self, text='Height:', font=("Lucida Grande", 12))
         self.lbl_height.grid(row=1, column=0, padx=5,
                              pady=5, sticky=tk.W)
 
         self.lbl_colors = ttk.Label(
-            self.root_frame, text='Colors:', font=("Lucida Grande", 12))
+            self, text='Colors:', font=("Lucida Grande", 12))
         self.lbl_colors.grid(row=2, column=0, padx=5,
                              pady=5, sticky=tk.W)
 
@@ -51,11 +45,11 @@ class Settings:
         # Define and Put Textboxes
 
         self.tb_width = ttk.Entry(
-            self.root_frame, width=10, textvariable=self.width_str)
+            self, width=10, textvariable=self.width_str)
         self.tb_height = ttk.Entry(
-            self.root_frame, width=10, textvariable=self.height_str)
+            self, width=10, textvariable=self.height_str)
         self.tb_colors = ttk.Entry(
-            self.root_frame, width=10, textvariable=self.colors_str)
+            self, width=10, textvariable=self.colors_str)
 
         self.tb_width.grid(row=0, column=1, padx=5,
                            pady=5, sticky=tk.N+tk.E)
@@ -64,12 +58,13 @@ class Settings:
         self.tb_colors.grid(row=2, column=1, padx=5,
                             pady=5, sticky=tk.E)
 
-        # Buttons
+        # Buttons Configuration and Placement
 
-        self.btn_ok = Round_Button(self.root_frame, **btn_small_style,
-                                   text="Ok")
-        self.btn_back = Round_Button(self.root_frame, **btn_small_style,
-                                     text="Back")
+        self.btn_ok = Round_Button(
+            self, **btn_small_style, text="Ok")
+        self.btn_back = Round_Button(
+            self, **btn_small_style,
+            text="Back", command=lambda: controller.show_frame("Main Menu"))
 
         self.btn_ok.grid(row=4, column=1, padx=5,
                          pady=5, sticky=tk.S+tk.E)
@@ -78,9 +73,3 @@ class Settings:
 
         btn_small_style['background'] = ttk.Style().lookup(
             'TFrame', 'background')
-
-        self.settings.update()
-        self.settings.minsize(self.settings.winfo_width(),
-                              self.settings.winfo_height())
-
-# settings.mainloop()
